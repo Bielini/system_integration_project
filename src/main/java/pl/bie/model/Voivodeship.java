5343b1029e1833fa6aec638aa5961a4394958e1f
Piotr B <piotr.bielawski@onet.pl>
2022-04-06 13:30:10 +0200

    model of voivodeship with partiesAmountByYear added

---
+++ src/main/java/pl/bie/model/Voivodeship.java
@@ -0,0 +1,19 @@
+package pl.bie.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Voivodeship {
+
+    private String name;
+    private Map<String,Integer> partiesAmountByYears ;
+
+
+}
